9c79b347033e14765c8508c6fcf34742b9d1f266
vipolus <tomkon1234@gmail.com>
2023-05-07 01:13:24 +0300

    Update

--- index.php
+++ index.php
@@ -2,25 +2,36 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="Static/css/style.css">
     <title>Library Management System</title>
+    <link rel="stylesheet" href="Static/css/style.css">
 </head>
 <body>
-   <h1>Welcome to School Library Management System</h1>
-   
-    <style>
-        div {
-          text-align: center;
-        }
-      </style>
-    <div class="imgcontainer">
-      <img src="Templates/Screenshot_3.png" alt="Avatar" class="avatar">
+    <div class="container">
+        <div class="top-bar">
+            <div class="buttons">
+                <a href="login.php" class="button">Login</a>
+                <a href="register.php" class="button">Register</a>
+            </div>
+        </div>
+        <div class="sidebar">
+            <div class="logo">
+                <img src="Templates/Screenshot_3.png" alt="Library Logo">
+            </div>
+            <nav class="menu">
+                <ul>
+                    <li><a href="#">Search Book</a></li>
+                    <li><a href="#">Lend a Book</a></li>
+                    <li><a href="#">Return a Book</a></li>
+                    <li><a href="#">Manage Users</a></li>
+                    <!-- Add more menu options as needed -->
+                </ul>
+            </nav>
+        </div>
+        <div class="main-content">
+            <h1>Welcome to the Library Management System</h1>
+            <!-- Add your main content here -->
+        </div>
     </div>
-  
-   
-
-
-
-
 </body>
 </html>
+
