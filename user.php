e004d7e46edf35b2d7eb9b72a64cb34360d2b79a
eladamou <128469763+eladamou@users.noreply.github.com>
2023-05-09 18:29:11 +0300

    Update user.php

--- user.php
+++ user.php
@@ -1,10 +1,25 @@
 <?php
 require_once('Book.php');
 session_start();
-    $user_type='SELECT Type FROM user WHERE Username = ?';
+$username = $_SESSION['username'];
+
+    $user_type='SELECT Type FROM User WHERE Username = ?';
     if ($user_type == "teacher") {
-        #see how many they have right now
+        $books_alrd_took='SELECT books_taken_temp FROM User WHERE Username = ?';
+        if($books_alrd_took == 1){
+            print("Naughty naughty!!");
+        }
+        else{
+            #gotoapprovedloaniguess??
+        }
     } else {
         #see how many they have right now
+        $books_alrd_took='SELECT books_taken_temp FROM User WHERE Username = ?';
+        if($books_alrd_took == 2){
+            print("Naughty naughty!!");
+        }
+        else{
+            #gotoapprovedloaniguess??
+        }
     }
 ?>
