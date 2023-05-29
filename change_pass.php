f6b38102b04e69dcf2714d5bc2569f20db4dccd2
eladamou <128469763+eladamou@users.noreply.github.com>
2023-05-29 14:55:45 +0300

    updates

---
+++ change_pass.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+require_once 'config.php';
+
+// Check if the user is logged in
+if (!isset($_SESSION['username'])) {
+    // Redirect the user or show an error message
+    header("Location: login.php");
+    exit;
+}
+
+// Get the username from the session
+$username = $_SESSION['username'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Process the form submission
+    $newPassword = $_POST['new_password'];
+
+    try {
+        // Update the user's password in the database
+        $pdo = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Hash the new password
+        $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+
+        // Update the password for the user
+        $stmt = $pdo->prepare("UPDATE User SET Password = ? WHERE Username = ?");
+        $stmt->execute([$hashedPassword, $username]);
+
+        // Redirect the user to a success page or show a success message
+        header("Location: change_pass_success.php");
+        exit;
+    } catch (PDOException $e) {
+        exit("Error: " . $e->getMessage());
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Change Password</title>
+</head>
+<body>
+    <h1>Change Password</h1>
+    <form method="POST" action="">
+        <label for="new_password">New Password:</label>
+        <input type="password" id="new_password" name="new_password" required>
+        <br>
+        <input type="submit" value="Change Password">
+    </form>
+</body>
+</html>
--- user_details.php
+++ user_details.php
@@ -29,6 +29,8 @@ if ($user) {
     if ($user['Type'] === "Teacher") {
         // Display the edit link for teachers
         //echo "<p><a href='edit_teacher.php'>Edit Teacher Data</a></p>";
+        echo "<a href='change_pass.php'><button>Change Password</button></a>";
+
         ?>
         <!DOCTYPE html>
         <html>
@@ -48,6 +50,7 @@ if ($user) {
 
                 <label for="age">Age:</label>
                 <input type="number" name="age" id="age" required value="<?php echo htmlspecialchars($user['Age']); ?>"> 
+                 
                 <input type="submit" value="Update">
                 
             </form>
@@ -96,6 +99,156 @@ if ($user) {
             }
         }
     }
+    if ($user['Type'] === "Library Operator") {
+        // Display the edit link for teachers
+        //echo "<p><a href='edit_teacher.php'>Edit Teacher Data</a></p>";
+        echo "<a href='change_pass.php'><button>Change Password</button></a>";
+
+        ?>
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Edit Library Operator Data</title>
+        </head>
+        <body>
+            <h1>Edit Library Operator Data</h1>
+            <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                <!-- Display the form fields for editing teacher data -->
+                <!-- You can customize the form fields based on your requirements -->
+                <label for="first_name">First Name:</label>
+                <input type="text" name="first_name" id="first_name" required value="<?php echo htmlspecialchars($user['First_Name']); ?>">
+
+                <label for="last_name">Last Name:</label>
+                <input type="text" name="last_name" id="last_name" required value="<?php echo htmlspecialchars($user['Last_Name']); ?>">
+
+                <label for="age">Age:</label>
+                <input type="number" name="age" id="age" required value="<?php echo htmlspecialchars($user['Age']); ?>"> 
+                
+            
+                <input type="submit" value="Update">
+                
+
+
+                
+            </form>
+        </body>
+        </html>
+        <?php
+        // Check if the form is submitted
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Retrieve the form fields
+            $first_name = $_POST['first_name'];
+            $last_name = $_POST['last_name'];
+            $age = $_POST['age'];
+
+            // Perform validation and sanitization on the submitted data
+            // You should validate and sanitize the data based on your specific requirements
+
+            // Update the teacher's data in the database
+            try {
+                require_once('config.php');
+
+                // Establish a connection to the database
+                $pdo = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                // Prepare the SQL statement to update the teacher's data
+                $stmt = $pdo->prepare("UPDATE User SET First_Name = :first_name, Last_Name = :last_name, Age = :age WHERE Username = :username");
+
+                // Bind the parameters
+                $stmt->bindParam(':first_name', $first_name);
+                $stmt->bindParam(':last_name', $last_name);
+                $stmt->bindParam(':age', $age);
+                $stmt->bindParam(':username', $username);
+
+                // Execute the SQL statement
+                $stmt->execute();
+
+                // Redirect the user to a success page or display a success message
+                // Example: header('Location: success.php');
+                exit;
+            } catch (PDOException $e) {
+                // Handle any errors that occurred during the database operation
+                echo "Error: " . $e->getMessage();
+                // You can redirect the user to an error page or display an error message
+                // Example: header('Location: error.php');
+                exit;
+            }
+        }
+    }
+    if ($user['Type'] === "Admin") {
+        // Display the edit link for teachers
+        //echo "<p><a href='edit_teacher.php'>Edit Teacher Data</a></p>";
+        echo "<a href='change_pass.php'><button>Change Password</button></a>";
+
+        ?>
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Edit Admin Data</title>
+        </head>
+        <body>
+            <h1>Edit Admin Data</h1>
+            <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                <!-- Display the form fields for editing teacher data -->
+                <!-- You can customize the form fields based on your requirements -->
+                <label for="first_name">First Name:</label>
+                <input type="text" name="first_name" id="first_name" required value="<?php echo htmlspecialchars($user['First_Name']); ?>">
+
+                <label for="last_name">Last Name:</label>
+                <input type="text" name="last_name" id="last_name" required value="<?php echo htmlspecialchars($user['Last_Name']); ?>">
+
+                <label for="age">Age:</label>
+                <input type="number" name="age" id="age" required value="<?php echo htmlspecialchars($user['Age']); ?>"> 
+                
+                <input type="submit" value="Update">
+            </form>
+        </body>
+        </html>
+        <?php
+        // Check if the form is submitted
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Retrieve the form fields
+            $first_name = $_POST['first_name'];
+            $last_name = $_POST['last_name'];
+            $age = $_POST['age'];
+
+            // Perform validation and sanitization on the submitted data
+            // You should validate and sanitize the data based on your specific requirements
+
+            // Update the teacher's data in the database
+            try {
+                require_once('config.php');
+
+                // Establish a connection to the database
+                $pdo = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                // Prepare the SQL statement to update the teacher's data
+                $stmt = $pdo->prepare("UPDATE User SET First_Name = :first_name, Last_Name = :last_name, Age = :age WHERE Username = :username");
+
+                // Bind the parameters
+                $stmt->bindParam(':first_name', $first_name);
+                $stmt->bindParam(':last_name', $last_name);
+                $stmt->bindParam(':age', $age);
+                $stmt->bindParam(':username', $username);
+
+                // Execute the SQL statement
+                $stmt->execute();
+
+                // Redirect the user to a success page or display a success message
+                // Example: header('Location: success.php');
+                exit;
+            } catch (PDOException $e) {
+                // Handle any errors that occurred during the database operation
+                echo "Error: " . $e->getMessage();
+                // You can redirect the user to an error page or display an error message
+                // Example: header('Location: error.php');
+                exit;
+            }
+        }
+    }
+    
     echo "<form method='POST' action='show_books_by_user.php'>";
     echo "<input type='hidden' name='username' value='$username'>";
     echo "<input type='submit' value='Show Books Loaned'>";
