0e21a2231cc1e698595f29eba492f8afc30fdfc9
eladamou <128469763+eladamou@users.noreply.github.com>
2023-06-02 11:00:50 +0300

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,40 +1,31 @@
-CREATE TRIGGER insert_reservation_trigger AFTER INSERT ON Reservation
-FOR EACH ROW
-BEGIN
-  INSERT INTO Reservation (School_id, User_id, Book_id, date_created, date_expired) 
-  SELECT :school_id, NEW.User_id, NEW.Book_id, CURRENT_TIMESTAMP, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 7 DAY)
-  FROM dual
-  WHERE NOT EXISTS (
-    SELECT 1
-    FROM Reservation
-    WHERE User_id = NEW.User_id AND Book_id = NEW.Book_id
-  );
-END;
 
+DELIMITER //
 
-
-
-
-/* Thanasis*/
-
-CREATE TRIGGER `Lib_Op` AFTER INSERT ON `user`
+CREATE TRIGGER `Lib_Op` AFTER INSERT ON `User`
  FOR EACH ROW IF NEW.Type = 'Library Operator' THEN
                             INSERT INTO School_Library_Operator (School_id) VALUES (NEW.School_id);
-                        END IF
+                        END IF//
+                      
+DELIMITER ;
 
 
- CREATE TRIGGER `User_Reservations` AFTER INSERT ON `reservation`
+
+DELIMITER //
+ CREATE TRIGGER `User_Reservations` AFTER INSERT ON `Reservation`
  FOR EACH ROW BEGIN
     UPDATE User
     SET reservations = reservations + 1
     WHERE User_id = NEW.User_id;
-END
+END//
+DELIMITER ;
 
 
-CREATE TRIGGER `Approve_History` AFTER UPDATE ON `user`
+DELIMITER //
+CREATE TRIGGER `Approve_History` AFTER UPDATE ON `User`
 FOR EACH ROW
 BEGIN
     IF NEW.Approved = '1' THEN
         INSERT INTO `Approve` (User_id) VALUES (NEW.User_id);
     END IF;
-END 
+END//
+DELIMITER ;
